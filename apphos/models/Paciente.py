dadb9c4484c1c9e731465fbb69cc54f7733f2c47
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-14 20:49:49 -0500

    Update Paciente.py
    se adiciona la tabla paciente

--- apphos/models/Paciente.py
+++ apphos/models/Paciente.py
@@ -9,6 +9,6 @@ class paciente(models.model):
 	Fecha_nto=models.CharField (max_length=20)
 	Direccion_pac=models.CharField (max_length=20)
 	Cedula_med=models.CharField (max_length=20)
-	Cedula_enfe=models.CharField (max_length=20)
-	Cedula_med = models.ForeignKey(Cedula_med.related_model)
-	FOREIGN KEY (Cedula_enfe) REFERENCES Enfermero(Cedula_enfe))
+	Cedula_enfe=  models.ForeignKey(Cedula_enfe.related_model'enfermero',on_delete=models.cascade)
+	Cedula_med =  models.ForeignKey(Cedula_med.related_model'medico',on_delete=models.cascade)
+	
