70325586c4401b3ad37a95224789016499741466
yulibahamon <yulibahamon13@gmail.com>
2022-09-15 23:56:47 -0500

    creacion moduls de yuliana

---
+++ apphos/models/Historia_clinica.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Historia_clinica(models.model):
+    
+    Id_historia=models.CharField (primary_key=true)
+	Diagnostico=models.CharField (max_length=1000)
+	Sugerencia=models.CharField (max_length=1000)
+	Cedula_paciente=  models.ForeignKey(Cedula_paciente.related_model'paciente',on_delete=models.cascade)
