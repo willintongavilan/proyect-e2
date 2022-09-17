c88af2abcd1572fe085bf14e4b1689e45ffc8bae
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-17 16:57:36 -0500

    Update serializers.py

--- apphos/serializers.py
+++ apphos/serializers.py
@@ -3,6 +3,21 @@ from rest_framework import serializers
 from .models import Paciente
 
 class pacenteserializer(serializers.ModelSerializer):
+     account = AccountSerializer()
     class Meta:
         model=Paciente
         fields='__all__'
+
+    def create(self, validated_data):
+        accountData = validated_data.pop('account')
+        userInstance = User.objects.create(**validated_data)
+        Account.objects.create(user=userInstance, **accountData)
+        return userInstance
+
+    def to_representation(self, obj):
+        user = User.objects.get(id=obj.id)
+        account = Account.objects.get(user=obj.id) 
+        return {
+            
+            }
+        }
