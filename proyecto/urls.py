affc0350102d1d4f5d8475ea2e4f366c93733c02
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-17 14:27:17 -0500

    Update urls.py

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from.router import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include(router.urls))
 ]
+#127.0.0.1:8000/api/Paciente
