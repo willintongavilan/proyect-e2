b6d2bc5d8843dd59788d13f9bd5d62258c13ebb5
Damlopezsu <113486617+Damlopezsu@users.noreply.github.com>
2022-09-17 21:03:31 -0500

    Create Familiar.py

---
+++ apphos/models/Familiar.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Familiar(models.model):
+    cedula_fam=models.CharField (primary_key=true)
+    nombre=models.CharField (max_length=20)
+    apellido_1=models.CharField (max_length=20)
+    apellido_2=models.CharField (max_length=20)
+    celular=models.CharField (max_length=10)
+    email=models.CharField (max_length=50)
+    direccion=models.CharField (max_length=30)
+    cedula_pac=models.CharField (max_length=10)
+    cedula_pac=  models.ForeignKey(Cedula_pac.related_model'Paciente',on_delete=models.cascade)
+
+	
