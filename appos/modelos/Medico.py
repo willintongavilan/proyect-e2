0ead0c1c75b088ee0766da5b2d1a6c13ba5fb419
Damlopezsu <113486617+Damlopezsu@users.noreply.github.com>
2022-09-17 20:07:28 -0500

    Delete Medico.py

--- appos/modelos/Medico.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Medico(models.model):
-    Cedula_med=models.CharField (primary_key=true)
-	Nombre=models.CharField (max_length=20)
-	Apellido_1=models.CharField (max_length=20)
-    Apellido_2=models.CharField (max_length=20)
-    
-	
