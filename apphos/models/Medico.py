baa9e9d5858ce6979e8db4a2720219fd81d894cc
Damlopezsu <113486617+Damlopezsu@users.noreply.github.com>
2022-09-17 21:02:32 -0500

    Create Medico.py

---
+++ apphos/models/Medico.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Medico(models.model):
+    cedula_med=models.CharField (primary_key=true)
+    nombre=models.CharField (max_length=20)
+    apellido_1=models.CharField (max_length=20)
+    apellido_2=models.CharField (max_length=20)
+    
+	
