47c22131b76c41a056dfc362ebe839d5bbc8cb68
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-19 16:46:33 -0500

    Update Paciente.py

--- apphos/models/Paciente.py
+++ apphos/models/Paciente.py
@@ -1,14 +1,15 @@
 from django.db import models
+from proyect-e2.apphos.models.Historia_clinica import Historia_clinica
 
 class paciente(models.model):
-    
-    Cedula_pac=models.CharField (primary_key=true)
-	Nombre=models.CharField (max_length=20)
-	Apellido_1=models.CharField (max_length=20)
-	Apellido_2=models.CharField (max_length=20)
-	Fecha_nto=models.CharField (max_length=20)
-	Direccion_pac=models.CharField (max_length=20)
-	Cedula_med=models.CharField (max_length=20)
-	Cedula_enfe=  models.ForeignKey(Cedula_enfe.related_model'enfermero',on_delete=models.cascade)
-	Cedula_med =  models.ForeignKey(Cedula_med.related_model'medico',on_delete=models.cascade)
-	
+     
+ cedula_pac=models.CharField (primary_key=True)
+ nombre = models.CharField (max_length=20)
+ apellido_1=models.CharField (max_length=20)
+ apellido_2=models.CharField (max_length=20)
+ fecha_nto=models.CharField (max_length=20)
+ direccion_pac=models.CharField (max_length=20)
+ cedula_med=models.CharField (max_length=20)
+ cedula_enfe=models.CharField (max_length=20)
+ cedula_med=  models.ForeignKey (cedula_med.related_model'medico',on_delete=models.cascade)
+ Historia_clinica=models.ForeignKey (Historia_clinica.related_model'Historia_clinica',on_delete=models.cascade)
