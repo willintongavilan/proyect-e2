9c926f9625ac412d301c3338e0cf5be684d53491
sanchezyopasa <113486047+sanchezyopasa@users.noreply.github.com>
2022-09-19 13:04:06 -0500

    Create Enfermero.py

---
+++ apphos/models/Enfermero.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Enfermero(models.model):
+    Cedula_enfe=models.CharField (primary_key=true) (max_length=10)
+	Nombre=models.CharField (max_length=20)
+	Apellido_1=models.CharField (max_length=20)
+	Apellido_2=models.CharField (max_length=20)
+	
