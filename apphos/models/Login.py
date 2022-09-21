9144882fbc9507b727d03f915479a9b12ff73622
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-20 20:14:16 -0500

    Update Login.py

--- apphos/models/Login.py
+++ apphos/models/Login.py
@@ -1,8 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.hashers import make_password
 
-class login(models.model):
+class UserManager(BaseUserManager):
 
-    Usuario=models.CharField (primary_key=true) (max_length=10)
-	Password=models.CharField (max_length=20)
-	Rol=models.CharField (max_length=20)
-	
+    def create_user(self, username, password=None):
+        """
+        Creates and saves a user with the given username and password.
+        """
+        if not username:
+            raise ValueError('Users must have an username')
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password):
+        """
+        Creates and saves a superuser with the given username and password.
+        """
+        user = self.create_user(
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class login(AbstractBaseUser, PermissionsMixin):
+
+    id = models.BigAutoField(primary_key=True)
+    username = models.CharField('Username', max_length = 15, unique=True)
+    password = models.CharField('Password', max_length = 256)
+ 
+
+    
+    def save(self, **kwargs):
+        some_salt = 'mMUj0DrIK6vgtdIYepkIxN'
+        self.password = make_password(self.password, some_salt)
+        super().save(**kwargs)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'username'
