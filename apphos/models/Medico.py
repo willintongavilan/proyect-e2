fc0ef80fc2f9b5851a72c6dbd43c7fdbe9c023c4
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-20 20:17:36 -0500

    Update Medico.py

--- apphos/models/Medico.py
+++ apphos/models/Medico.py
@@ -4,6 +4,8 @@ class Medico(models.model):
     cedula_med=models.CharField (primary_key=true)
     nombre=models.CharField (max_length=20)
     apellido_1=models.CharField (max_length=20)
-    apellido_2=models.CharField (max_length=20)
+    apellido_2=models.CharField (max_length=20) 
+    login =models.ForeignKey (login.related_model'login',on_delete=models.cascade)	
+	
     
 	
