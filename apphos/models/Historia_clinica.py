9fbb4a36a145610d92fd84867fcc2453a3c2e55c
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-20 20:09:23 -0500

    Update Historia_clinica.py

--- apphos/models/Historia_clinica.py
+++ apphos/models/Historia_clinica.py
@@ -5,4 +5,4 @@ class Historia_clinica(models.model):
     Id_historia=models.CharField (primary_key=true)
 	Diagnostico=models.CharField (max_length=1000)
 	Sugerencia=models.CharField (max_length=1000)
-	Cedula_paciente=  models.ForeignKey(Cedula_paciente.related_model'paciente',on_delete=models.cascade)
+	Cedula_paciente=  models.ForeignKey(Cedula_paciente.related_model'Paciente',on_delete=models.cascade)
