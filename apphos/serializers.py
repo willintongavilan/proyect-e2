2a8eb6a72ada743493e7d37f48b87baf683fdc8a
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-20 20:39:57 -0500

    Update serializers.py

--- apphos/serializers.py
+++ apphos/serializers.py
@@ -1,23 +1,9 @@
 from dataclasses import fields
 from rest_framework import serializers
-from .models import Paciente
+from apphos.models.Paciente import Paciente
 
 class pacenteserializer(serializers.ModelSerializer):
-     account = AccountSerializer()
-    class Meta:
+     
+ class Meta:
         model=Paciente
-        fields='__all__'
-
-    def create(self, validated_data):
-        accountData = validated_data.pop('account')
-        userInstance = User.objects.create(**validated_data)
-        Account.objects.create(user=userInstance, **accountData)
-        return userInstance
-
-    def to_representation(self, obj):
-        user = User.objects.get(id=obj.id)
-        account = Account.objects.get(user=obj.id) 
-        return {
-            
-            }
-        }
+        fields=('cedula_pac,nombre,apellido_1,apellido_2, fecha_nto,direccion_pac,cedula_med,cedula_enfe,'),
