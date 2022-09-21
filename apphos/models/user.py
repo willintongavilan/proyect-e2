0e9db124e392b998ebf54f7132358eb22b039d2f
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-20 20:12:57 -0500

    Update user.py

--- apphos/models/user.py
+++ apphos/models/user.py
@@ -33,8 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     id = models.BigAutoField(primary_key=True)
     username = models.CharField('Username', max_length = 15, unique=True)
     password = models.CharField('Password', max_length = 256)
-    name = models.CharField('Name', max_length = 30)
-    email = models.EmailField('Email', max_length = 100)
+ 
 
     
     def save(self, **kwargs):
