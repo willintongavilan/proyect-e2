523cce113f7b6bc03b9609411ba6c17d13c9e8bd
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-24 07:58:22 -0500

    Update Enfermero.py

--- apphos/models/Enfermero.py
+++ apphos/models/Enfermero.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Enfermero(models.model):
     cedula_enfe=models.CharField (primary_key=true) (max_length=10)
-	nombre=models.CharField (max_length=20)
-	apellido_1=models.CharField (max_length=20)
-	apellido_2=models.CharField (max_length=20)
-	numtel_enfe=models.CharField (max_length=10)
-	login =models.ForeignKey (login.related_model'login',on_delete=models.cascade)
+    nombre=models.CharField (max_length=20)
+    apellido_1=models.CharField (max_length=20)
+    apellido_2=models.CharField (max_length=20)
+    numtel_enfe=models.CharField (max_length=10)
+    login =models.ForeignKey (login.related_model'login',on_delete=models.cascade)
+	
 	
