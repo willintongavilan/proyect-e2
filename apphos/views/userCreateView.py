b9236a72df4062fddec6aa0061261415d96f1fdb
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-24 08:20:30 -0500

    Update userCreateView.py

--- apphos/views/userCreateView.py
+++ apphos/views/userCreateView.py
@@ -1,28 +1,16 @@
-from rest_framework.views import APIView
+from rest_framework import status, views
 from rest_framework.response import Response
-from rest_framework import status
-from apphos.Serializers.ser_auxiliar import Auxiliar
-from apphos.models.auxiliar import Auxiliar
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-class detailauxiliarview(APIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
-      
-    def get(self, request, pk, format=None):
-        snippet = Auxiliar.objects.get(pk=pk)
-        serializer = Auxiliar(snippet)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        snippet = Auxiliar.objects.get(pk=pk)
-        serializer = Auxiliar(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        snippet = Auxiliar.objects.get(pk=pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+from apphos.serializers.userSerializer import UserSerializer
+class UserCreateView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        tokenData = {"username":request.data["username"],
+        "password":request.data["password"]}
+        tokenSerializer = TokenObtainPairSerializer(data=tokenData)
+        tokenSerializer.is_valid(raise_exception=True)
+ 
+        return Response(tokenSerializer.validated_data, status=status.HTTP_201_CREATED)
