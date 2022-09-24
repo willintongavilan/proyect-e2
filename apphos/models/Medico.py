c48c0e906851e5b1302cce1ba4a946e4e9f70128
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-24 08:02:07 -0500

    Update Medico.py

--- apphos/models/Medico.py
+++ apphos/models/Medico.py
@@ -1,12 +1,11 @@
 from django.db import models
 
 class Medico(models.model):
-    cedula_med=models.CharField (primary_key=true)
+    cedula_med=models.CharField (primary_key=True)
     nombre=models.CharField (max_length=20)
     apellido_1=models.CharField (max_length=20)
     apellido_2=models.CharField (max_length=20) 
     numtel_med=models.CharField (max_length=10)
-    login =models.ForeignKey (login.related_model'login',on_delete=models.cascade)	
-	
+    login =models.ForeignKey (login.related_model'login',on_delete=models.cascade)
     
 	
