efe403d08fff001dd5ba135c7abbdd3fcdbf7309
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-24 08:10:21 -0500

    Update Ser_Auxiliar.py

--- apphos/serializadores/Ser_Auxiliar.py
+++ apphos/serializadores/Ser_Auxiliar.py
@@ -1,9 +1,9 @@
 from dataclasses import fields
 from rest_framework import serializers
-from apphos.models.Auxiliar import Auxiliar
+from apphos.models.auxiliar import Auxiliar
 
 class pacenteserializer(serializers.ModelSerializer):
      
  class Meta:
         model=Auxiliar
-        fields=('cedula_aux,nombre,apellido_1,apellido_2,numtel_aux,'),
+        fields=('cedula_aux','nombre,apellido_1','apellido_2','numtel_aux','login')
