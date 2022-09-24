d4561b076457ded82357d79fbc1b9f3035394041
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-24 07:59:42 -0500

    Update Historia_clinica.py

--- apphos/models/Historia_clinica.py
+++ apphos/models/Historia_clinica.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Historia_clinica(models.model):
     
-    id_historia=models.CharField (primary_key=true)
-	diagnostico=models.CharField (max_length=1000)
-	sugerencia=models.CharField (max_length=1000)
-	cedula_paciente=  models.ForeignKey(Cedula_paciente.related_model'Paciente',on_delete=models.cascade)
-	login =models.ForeignKey (login.related_model'login',on_delete=models.cascade)
+   id_historia=models.CharField (primary_key=true)
+   diagnostico=models.CharField (max_length=1000)
+   sugerencia=models.CharField (max_length=1000)
+   cedula_paciente=  models.ForeignKey(cedula_paciente.related_model'paciente',on_delete=models.cascade)
+   login =models.ForeignKey (login.related_model'login',on_delete=models.cascade)
