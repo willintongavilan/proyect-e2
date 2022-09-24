bc486f2a655204b8e4df34956fc82e173e54a1b0
willintongavilan <113482365+willintongavilan@users.noreply.github.com>
2022-09-24 08:07:10 -0500

    Update Signos_vitales.py

--- apphos/models/Signos_vitales.py
+++ apphos/models/Signos_vitales.py
@@ -1,15 +1,16 @@
+from tokenize import Name
 from django.db import models
 
 class paciente(models.model):
     
-    Id_signos=models.CharField (primary_key=true)
-	Fecha=models.CharField (max_length=10)
-	Hora=models.CharField (max_length=10)
-	Oximetria=models.CharField (max_length=10)
-	Frec_respirato=models.CharField (max_length=10)
-	Frec_cardiaca=models.CharField (max_length=20)
-	Temperatura=models.CharField (max_length=10)
-        Presion_arterial=models.CharField (max_length=10)
-        Glisemia=models.CharField (max_length=10)
-	Cedula_paciente=  models.ForeignKey(Cedula_paciente.related_model'paciente',on_delete=models.cascade)
+ Id_signos=models.CharField (primary_key=True)
+ fecha=models.CharField (max_length=10)
+ Hora=models.CharField (max_length=10)
+ Oximetria=models.CharField (max_length=10)
+ Frec_respirato=models.CharField (max_length=10)
+ Frec_cardiaca=models.CharField (max_length=20)
+ Temperatura=models.CharField (max_length=10)
+ presion_arterial=models.CharField (max_length=10)
+ Glisemia=models.CharField (max_length=10)
+ cedula_paciente=  models.ForeignKey(paciente,related_name='paciente',on_delete=models.cascade
 
